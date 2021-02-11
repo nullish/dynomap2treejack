042dd098ed254d8e4850d1b0500966ac300be44a
Nullish <richerde43@gmail.com>
2021-02-11 23:25:48 +0000

    List node names and levels

--- js/dynomap2treejack.js
+++ js/dynomap2treejack.js
@@ -1,3 +1,11 @@
 const dynoJack = () => {
-	
+	const sitemap = document.getElementsByTagName("a");
+	let linkList = "";
+	for (let e of sitemap) { 
+		let linkClass = e.getAttribute("class");
+		let level = linkClass.match(/[0-9]{1,}/)[0];
+		let linkName = e.innerText;
+		linkList += `${level} ${linkName}\n`;
+	}
+	console.log(linkList);
 }
