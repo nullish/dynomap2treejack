ef9aa22dff334f66989acc79dbc4808ddb554bfe
Nullish <richerde43@gmail.com>
2021-02-11 23:31:45 +0000

    Indent links with comma deliniters to indicate levels

--- js/dynomap2treejack.js
+++ js/dynomap2treejack.js
@@ -1,11 +1,19 @@
 const dynoJack = () => {
+	/* 
+	Parse DynoMap produced HTML sitemap and output indented text equivalent using comma delimiters
+	to indicate levels in nav tree.
+	*/
 	const sitemap = document.getElementsByTagName("a");
 	let linkList = "";
 	for (let e of sitemap) { 
 		let linkClass = e.getAttribute("class");
 		let level = linkClass.match(/[0-9]{1,}/)[0];
+		let linkIndent = "";
+		for (let i = 0; i < level; i++) {
+			linkIndent += ",";
+		}
 		let linkName = e.innerText;
-		linkList += `${level} ${linkName}\n`;
+		linkList += `${linkIndent}${linkName}\n`;
 	}
 	console.log(linkList);
 }
