ff09274384dd6537776ab1e63053a1afe1d48351
Nullish <richerde43@gmail.com>
2021-02-12 17:13:09 +0000

    Output tab delimited text sitemap from file upload

--- assets/dynomap2treejack.js
+++ assets/dynomap2treejack.js
@@ -4,31 +4,49 @@
 	*/
 
 	document.querySelector(".get-sitemap").addEventListener("click", () => {
-    fetch("https://gist.githubusercontent.com/nullish/753bf8b435a84681d12a786b4696911c/raw/9b0e902bcb8d52922189e22bb18f8e133cbe389e/dynomap-test.html")
-        .then((response) => response.text())
-        .then((text) => {
-        	console.log(text);
-            const sitemap = document.implementation.createHTMLDocument("Sitemap").documentElement;
-            sitemap.innerHTML = text;
+		const file = document.getElementById('fi').files[0];
+		const reader = new FileReader();
+		reader.onload = function(event) {
+			const text = event.target.result;
+			const sitemap = document.implementation.createHTMLDocument("Sitemap").documentElement;
+			sitemap.innerHTML = text;
             // document.querySelector(".element_on_main_page").textContent = otherDoc.querySelector(".awesome_external_element").textContent;
             const siteLinks = sitemap.getElementsByTagName("a");
-            console.log(sitemap);
-	let linkList = "";
-	for (let e of siteLinks) { 
-		let linkClass = e.getAttribute("class");
-		let level = linkClass.match(/[0-9]{1,}/)[0];
-		let linkIndent = "";
-		for (let i = 0; i < level; i++) {
-			linkIndent += ",";
-		}
-		let linkName = e.innerText;
-		linkList += `${linkIndent}${linkName}\n`;
-	}
-	console.log(linkList);
-        });
-});
+            let linkList = "";
+            for (let e of siteLinks) { 
+            	let linkClass = e.getAttribute("class");
+            	let level = linkClass.match(/[0-9]{1,}/)[0];
+            	let linkIndent = "";
+            	for (let i = 0; i < level; i++) {
+            		linkIndent += "\t";
+            	}
+            	let linkName = e.innerText;
+            	linkList += `${linkIndent}${linkName}\n`;
+            }
+            document.getElementById("node-list").innerText = linkList;
+        }
+        reader.readAsText(file)
+    });
 
-	
+    document.querySelector(".copy-text").addEventListener("click", () => {
+    	/* Get the text field */
+  const copyText = document.getElementById("node-list");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+    })
+
+/*
+
+
+*/
 
 	/* ******* TODO
 
