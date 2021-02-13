cee64eeaa57cc605e3447d052c861a3e849592d2
Nullish <richerde43@gmail.com>
2021-02-13 14:13:00 +0000

    Revert to working version

--- assets/dynomap2treejack.js
+++ assets/dynomap2treejack.js
@@ -27,27 +27,23 @@
                 let rx = new RegExp(removeText);
                 linkName = linkName.replace(rx, '');
               }
-            	linkList += `${linkIndent}${linkName}\n`;
+              linkList += `${linkIndent}${linkName}\n`;
             }
             document.getElementById("node-list").innerText = linkList;
-        }
-        reader.readAsText(file)
-    });
+          }
+          reader.readAsText(file)
+        });
 
-    document.querySelector(".copy-text").addEventListener("click", () => {
-    	/* Get the text field */
-  const copyText = document.getElementById("node-list");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-    })
+  document.querySelector("#copy-text").addEventListener("click", () => {
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+  })
 
 /*
 
